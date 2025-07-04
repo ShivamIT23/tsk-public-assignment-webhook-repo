d367b77ce8ce932dd4d11de9fea764b4e5e581ca
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2025-07-04 16:04:29 +0530

    Added index 3

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -31,6 +31,7 @@ def webhook():
     event_type = request.headers.get("X-GitHub-Event")
     timestamp = datetime.utcnow().isoformat() + "Z"
     author = data.get("sender", {}).get("login", "Unknown")
+    print(f"[DEBUG] Webhook received: {event_type} by {author} at {timestamp}")
 
     if event_type == "push":
         to_branch = data.get("ref", "").split("/")[-1]
@@ -73,7 +74,7 @@ def webhook():
 
 @webhook_bp.route("/events", methods=["GET"])
 def get_events():
-    events = list(events_collection.find().sort("_id", -1).limit(10))
+    events = list(events_collection.find().sort("timestamp", -1).limit(10))
     for e in events:
         e["_id"] = str(e["_id"])
     return jsonify(events)
